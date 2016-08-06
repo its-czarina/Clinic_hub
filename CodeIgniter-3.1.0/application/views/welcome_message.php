c7002e6a2e91886237bbf3137701bf89ee48eb20
HowDoIGitHelp <bbadoodles@gmail.com>
2016-08-07 05:58:32 +0800

    welcome message commit

--- CodeIgniter-3.1.0/application/views/welcome_message.php
+++ CodeIgniter-3.1.0/application/views/welcome_message.php
@@ -1,27 +1,95 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?>
-<!DOCTYPE html>
-<html lang="en">
+?><html>
 <head>
-	<meta charset="utf-8">
-	<title>Welcome to ClinicAll!</title>
+	<link rel="stylesheet" type="text/css" href="/xampp/htdocs/CAll/CodeIgniter-3.1.0/application/assets/styles/global.css">
 </head>
 <body>
+	<?php echo validation_errors(); ?>
 
-<div>
-	<h1>Welcome to ClinicAll!</h1>
-
-	<div>
-		<a href="update_info">UPDATE YOUR MEDICAL INFORMATION</a><br>
-		<a href="schedule_checkup">SCHEDULE A CHECKUP</a><br>
-		<a href="search_doctor">SEARCH FOR A DOCTOR</a><br>
-		<a href="view_chart">VIEW YOUR CHART</a><br>
-		<a href="logout">LOGOUT</a><br>
-	</div>
-
+	<?php echo form_open('home'); ?>
+	<nav>
+	<div class="nav-container">
 	
-</div>
+	</div>
+	</nav>
 
+	<div class="hidden-container">
+	<div>
+	<div class="logo">
+	<img src="C:\Program Files\xampp\htdocs\xampp\htdocs\CAll\images\logo_y.png">
+	</div>
+	<div class="right-align nav-button">
+	<input type="submit" class="global-button animate btn" value="LOG-OUT" />
+	</div>
+	</div>
+	<form>
+	<div class="card">
+	<div class="card-title">
+	Services
+	</div>
+	<div class="card-content">
+	
+	<div class="row">
+	<div class="col leftmost">
+	<div class="general-button btn middle-center">
+	<div class="leftified">
+	<a href="update_info">UPDATE YOUR MEDICAL INFORMATION</a>
+	</div>
+	<div class="rightified">
+	<img class="button-icon" src="C:\Program Files\xampp\htdocs\xampp\htdocs\CAll\images\tablet.png">
+	</div>
+	</div>
+	</div>
+	<div class="col rightmost">
+	<div class="general-button btn middle-center">
+	<div class="leftified">
+	<a href="schedule_checkup">SCHEDULE A CHECKUP</a>
+	</div>
+	<div class="rightified">
+	<img class="button-icon" src="C:\Program Files\xampp\htdocs\xampp\htdocs\CAll\images\stethoscope.png">
+	</div>	
+	</div>
+	</div>
+	</div>
+	<div class="row">
+	<div class="col">
+	<div class="general-button btn middle-center">
+	<div class="leftified">
+	<a href="view_chart">VIEW YOUR CHART</a>
+	</div>
+	<div class="rightified">
+	<img class="button-icon" src="C:\Program Files\xampp\htdocs\xampp\htdocs\CAll\images\medical-history.png">
+	</div>
+	</div>
+	</div>
+	</div>
+	</div>
+	</div>
+	
+	</form>
+	</div>
+	<div class="footer">
+	<div class="col footer-item">
+	<div class="footer-title">
+	About
+	</div>
+	<div class="footer-content">
+	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+	</div>
+	</div>
+	<div class="col footer-item">
+	<div class="footer-title">
+	Contact
+	</div>
+	<div class="footer-content">
+	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+	</div>
+	</div>
+	</div>
+	</div>
+	<div class="copyright">
+	<a>ClinicAll 2016</a>
+	</div>
 </body>
 </html>
