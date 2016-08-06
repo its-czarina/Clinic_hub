96a89260f451deae64d95f2042c916ce12c5c0be
HowDoIGitHelp <bbadoodles@gmail.com>
2016-08-07 06:08:58 +0800

    schedule

--- CodeIgniter-3.1.0/application/views/schedule.php
+++ CodeIgniter-3.1.0/application/views/schedule.php
@@ -1,30 +1,98 @@
 <html>
-<head></head>
+<head>
+    <link rel="stylesheet" type="text/css" href="../assets/styles/global.css">
+</head>
 <body>
         <?php echo validation_errors(); ?>
 
-        <?php echo form_open('home/schedule_checkup'); ?>
-            Date: <br>
-            <input name="date" type="date"> <br>
-            Query: <br>
-            <textarea name="query"></textarea>
-            <br>
-            <div id="search">
-            Doctor: <br>
-            <select>
-                <option>Dentist</option>
-                <option>OB-GYN</option>
-                <option>Oncologist</option>
-            </select>
-            <input type="text" id="search"><button type="button" onclick="document.getElementById('search').value='Heavenly Librando'">Search</button> <br>
+        <?php echo form_open('home'); ?>
+        <nav>
+            <div class="nav-container">
+                
             </div>
-            <button type="button" onclick="document.getElementById('clinic_name').style.visibility='visible';document.getElementById('search').style.visibility='hidden';">Find me a Doctor!</button><br>
-            <select type="hidden" style="visibility:hidden;" id="clinic_name" size = 5>
-                <option title="Dr. Marlou Lumapas Clinic">Dr. Marlou Lumapas</option>
-                <option title="Dr. Repro Optima Clinic">Repro Optima</option>
-                <option title="Aventus Medical Clinic">Aventus Medical Clinic</option>
-            </select>
-            <div style="margin-top:20px"><input type="Submit" value="Submit" /></div>
+        </nav>
+
+        <div class="hidden-container">
+            <div>
+                <div class="logo">
+                    <img src="C:\Program Files\xampp\htdocs\xampp\htdocs\CAll\images\logo_y.png">
+                </div>
+                <div class="right-align nav-button">
+                    <input type="submit" class="global-button animate btn" value="LOG-OUT" />
+                </div>
+            </div>
+        <form>
+        <div class="card">
+            <div class="card-title">
+                Schedule an Appointment
+            </div>
+            <div class="card-content">
+                
+                <div class="row">
+                    <div class="col leftmost">
+                        <label>Date </label>
+                        <input name="date" type="date">
+                    </div>
+                    <div class="col rightmost">
+                        <label>Clinic Service </label>
+                        <input name="lastname" type="text"> 
+                    </div>
+                </div>
+                <div class="row">
+                    <label>Tell the doctor something</label>
+                    <textarea name="query"></textarea>
+                </div>
+                <div class="row">
+                    <div class="col leftmost">
+                        <label>Specialization </label>
+                        <select>
+                            <option>Dentist</option>
+                            <option>OB-GYN</option>
+                            <option>Oncologist</option>
+                        </select>
+                    </div>
+                    <div class="col rightmost">
+                        <label>Search a Doctor/Clinic </label>
+                        <div class="search">
+                            <div class="leftified tight">
+                                <input type="text" id="search">
+                            </div>
+                            <div class="rightified absolute">
+                                <img src="C:\Program Files\xampp\htdocs\xampp\htdocs\CAll\images\magnifying-glass.png" class="search-icon" onclick="document.getElementById('search').value='Heavenly Librando'">
+                            </div>
+                        </div>
+            
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <div class="right-align">
+            <input type="submit" class="global-button animate btn" value="SCHEDULE" />
+        </div>
         </form>
+    </div>
+    <div class="footer">
+            <div class="col footer-item">
+                <div class="footer-title">
+                    About
+                </div>
+                <div class="footer-content">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </div>
+            </div>
+            <div class="col footer-item">
+                <div class="footer-title">
+                    Contact
+                </div>
+                <div class="footer-content">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        <a>ClinicAll 2016</a>
+    </div>
 </body>
 </html>
