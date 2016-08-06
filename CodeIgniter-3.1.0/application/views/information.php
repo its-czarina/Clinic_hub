73da3fe78d4d335688a21b90d4883c5d718abf59
HowDoIGitHelp <bbadoodles@gmail.com>
2016-08-07 07:38:15 +0800

    View Record

--- CodeIgniter-3.1.0/application/views/information.php
+++ CodeIgniter-3.1.0/application/views/information.php
@@ -1,37 +1,130 @@
 <html>
-	<head>
-		<style>
-			.Month{
-				margin: 5px;
-			}
+<head>
+    <link rel="stylesheet" type="text/css" href="../assets/styles/global.css">
+</head>
+<body>
+        <?php echo validation_errors(); ?>
 
-		</style>
-	</head>
-	<body>
-		<!-- <button> July 2016 </button>
-		<button> June 2016 </button> -->
+        <?php echo form_open('home'); ?>
+        <nav>
+            <div class="nav-container">
+                
+            </div>
+        </nav>
 
-		<div class="Month" id="Aug2016">
-			<div class="date">August 1, 2015</div>
-			<div class="title">Cervical Check Up</div>
-			<div class="clinic">Cervical Clinic</div>
-			<div class="doctor">Dr. Fe del Mundo</div>
-			<div class="feedback">
-				Enter feedback:<br>
-				<textarea id="dis" disabled='FALSE' rows='3' cols='50'></textarea>
-				<button>Submit</button>
-			</div>
-		</div>
-		<div class="Month" id="July2016">
-			<div class="date">July 8, 2015</div>
-			<div class="title">EENT Check Up</div>
-			<div class="clinic">Aventus Medical Clinic</div>
-			<div class="doctor">Dr. Peter Munoz</div>
-			<div class="feedback">
-				Feedback: <br>
-				<textarea disabled="TRUE" rows='3' cols='50'>Very good experience!</textarea>
-			</div>
-		</div>
-
-	</body>
+        <div class="hidden-container">
+            <div>
+                <div class="logo">
+                    <img src="C:\Program Files\xampp\htdocs\xampp\htdocs\CAll\images\logo_y.png">
+                </div>
+                <div class="right-align nav-button">
+                    <input type="submit" class="global-button animate btn" value="LOG-OUT" />
+                </div>
+            </div>
+        <form>
+        <div class="card">
+            <div class="card-title">
+                August 1, 2016
+            </div>
+            <div class="card-content">
+                <div class="row">
+                        Cervical Clinic
+                </div>
+                <div class="row">
+                    <iframe
+                        class="map"
+                        frameborder="0"
+                        src="https://www.google.com/maps/embed/v1/search?key=AIzaSyAo5z_MhZ-VaQgwGhmiggLR2qVV3KkXXXk&q=Dr+Fe+del+Mundo" allowfullscreen>
+                    </iframe>
+                </div>
+                <div class="row">
+                    <label>Leave a Feedback</label>
+                    <textarea name="query"></textarea>
+                </div>
+                <div class="row">
+                    <div class="col leftmost">
+                        <label>Examiner </label>
+                        <div class="strong-text">Dr. Fe del Mundo</div>
+                    </div>
+                    <div class="col rightmost">
+                        <label>Rate this clinic </label>
+                                <div class="strong-text">
+                                    <div class="rating">
+                                        <span>&#x2606</span><span>&#x2606</span><span>&#x2606</span><span>&#x2606</span><span>&#x2606</span>
+                                    </div>
+                                </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <div class="right-align">
+            <input type="submit" class="global-button animate btn" value="RATE" />
+        </div>
+        </form>
+        <form>
+        <div class="card">
+            <div class="card-title">
+                July 8,2015
+            </div>
+            <div class="card-content">
+                <div class="row">
+                    EENT Check Up
+                </div>
+                <div class="row">
+                    <iframe
+                        class="map"
+                        frameborder="0"
+                        src="https://www.google.com/maps/embed/v1/search?key=AIzaSyAo5z_MhZ-VaQgwGhmiggLR2qVV3KkXXXk&q=Aventus+Medical+Clinic" allowfullscreen>
+                    </iframe>
+                </div>
+                <div class="row">
+                    <label>Leave a Feedback</label>
+                    <textarea name="query"></textarea>
+                </div>
+                <div class="row">
+                    <div class="col leftmost">
+                        <label>Examiner </label>
+                        <div class="strong-text">Dr. Peter Munoz</div>
+                    </div>
+                    <div class="col rightmost">
+                        <label>Rate this clinic </label>
+                                <div class="strong-text">
+                                    <div class="rating">
+                                        <span>&#x2606</span><span>&#x2606</span><span>&#x2606</span><span>&#x2606</span><span>&#x2606</span>
+                                    </div>
+                                </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <div class="right-align">
+            <input type="submit" class="global-button animate btn" value="RATE" />
+        </div>
+        </form>
+    </div>
+    <div class="footer">
+            <div class="col footer-item">
+                <div class="footer-title">
+                    About
+                </div>
+                <div class="footer-content">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </div>
+            </div>
+            <div class="col footer-item">
+                <div class="footer-title">
+                    Contact
+                </div>
+                <div class="footer-content">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        <a>ClinicAll 2016</a>
+    </div>
+</body>
 </html>
